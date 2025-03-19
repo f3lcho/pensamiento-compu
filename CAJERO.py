a0bf9046cdfc3427ccc239f14d01e94879040718
Fernando Flores <jeatzflores.2019@gmail.com>
2025-03-19 11:27:57 -0600

    Add files via upload

---
+++ CAJERO.py
@@ -0,0 +1,21 @@
+saldo = int(3000)
+cont = int(1)
+
+while True:
+    retirar = int(input("Ingrese cantidad a retirar: "))
+    retiro = saldo-retirar
+    
+    if retirar == 0:
+        print("gracias por usar el programa")
+        break
+    if retiro < 0:
+        print("Cantidad Insuficiente, ingrese otra cantidad valida")
+        cont += 1 
+    if cont == 4:
+        print("intentos terminados")
+        break
+    if retiro >= 0:
+        print("Operacion valida")
+        print("Su saldo restante es de ",retiro)
+        break
+    
